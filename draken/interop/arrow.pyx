653a42e09b9e64a43c99db00f1ca6bd6afa0a5c8
joocer <justin.joyce@joocer.com>
2025-09-13 10:56:48 +0100

    code restructuring

--- draken/interop/arrow.pyx
+++ draken/interop/arrow.pyx
@@ -15,7 +15,7 @@ cdef void release_arrow_schema(ArrowSchema* schema) noexcept:
     free(schema)
 
 cdef void expose_draken_fixed_as_arrow(
-    DrakenFixedBuffer* col,
+    DrakenFixedBuffer* vec,
     ArrowArray** out_array,
     ArrowSchema** out_schema,
 ):
@@ -25,7 +25,7 @@ cdef void expose_draken_fixed_as_arrow(
     out_schema[0] = schema
 
     # Fill ArrowArray
-    arr.length = col.length
+    arr.length = vec.length
     arr.null_count = -1
     arr.offset = 0
     arr.n_buffers = 2
@@ -36,16 +36,63 @@ cdef void expose_draken_fixed_as_arrow(
     arr.private_data = NULL
 
     arr.buffers = <const void**>malloc(2 * sizeof(void*))
-    arr.buffers[0] = <const void*>col.null_bitmap
-    arr.buffers[1] = col.data
+    arr.buffers[0] = <const void*>vec.null_bitmap
+    arr.buffers[1] = vec.data
 
     # Fill ArrowSchema
     schema.format = b"l"
     schema.name = NULL
     schema.metadata = NULL
-    schema.flags = ARROW_FLAG_NULLABLE if col.null_bitmap != NULL else 0
+    schema.flags = ARROW_FLAG_NULLABLE if vec.null_bitmap != NULL else 0
     schema.n_children = 0
     schema.children = NULL
     schema.dictionary = NULL
     schema.release = release_arrow_schema
     schema.private_data = NULL
+
+
+
+
+
+
+
+import pyarrow
+from libc.stdint cimport intptr_t, uint8_t
+
+from draken.vectors.int64_vector cimport Int64Vector
+from draken.core.buffers cimport DrakenFixedBuffer, DRAKEN_INT64
+
+cpdef object vector_from_arrow(object array):
+    pa_type = array.type
+    if pa_type.equals(pyarrow.int64()):
+        return _wrap_int64(array)
+    # TODO: add float64, bool, string, etc.
+    raise NotImplementedError(f"from_arrow: unsupported type {pa_type}")
+
+cdef Int64Vector _wrap_int64(object array):
+    cdef Int64Vector vec = Int64Vector(0, True)   # wrap=True: no alloc
+    vec.ptr = <DrakenFixedBuffer*> malloc(sizeof(DrakenFixedBuffer))
+    if vec.ptr == NULL:
+        raise MemoryError()
+    vec.owns_data = False
+
+    cdef object bufs = array.buffers()
+    cdef intptr_t base_ptr = <intptr_t> bufs[1].address
+    cdef size_t itemsize = 8
+    cdef Py_ssize_t offset = array.offset
+    cdef intptr_t nb_addr
+
+    vec.ptr.type = DRAKEN_INT64
+    vec.ptr.itemsize = itemsize
+    vec.ptr.length = <size_t> len(array)
+
+    cdef intptr_t addr = base_ptr + offset * itemsize
+    vec.ptr.data = <void*> addr
+
+    if bufs[0] is not None:
+        nb_addr = bufs[0].address
+        vec.ptr.null_bitmap = <uint8_t*> nb_addr
+    else:
+        vec.ptr.null_bitmap = NULL
+
+    return vec
