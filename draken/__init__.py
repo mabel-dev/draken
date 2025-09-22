9c1adf7ec61718047d2b901326d20fa5fee431fe
joocer <justin.joyce@joocer.com>
2025-09-22 22:24:28 +0100

    fix version file

--- draken/__init__.py
+++ draken/__init__.py
@@ -1,4 +1,8 @@
+from draken.__version__ import __author__
+from draken.__version__ import __build__
+from draken.__version__ import __version__
+
 from draken.morsels.morsel import Morsel
 from draken.vectors.vector import Vector
 
-__all__ = ("Vector", "Morsel")
+__all__ = ("Vector", "Morsel", "__version__", "__build__", "__author__")
